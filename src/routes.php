b7c136265fae60fc10ef1457388e83eb47933fa4
Andrew huggins <andrewhuggins@gmail.com>
2016-08-28 22:56:13 -0400

    mistyped Controller instead of Controllers in routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
 
-Route::get('blog/', 'ArtisanCMS\Blog\Http\Controller\BlogController@index');
+Route::get('blog/', 'ArtisanCMS\Blog\Http\Controllers\BlogController@index');
 
 Route::get('blog/{slug?}', 'ArtisanCMS\Blog\Http\Controllers\BlogController@show');
