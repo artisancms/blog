28fc8c134e7191b05a479bdfeb49d3dfedd429be
Andrew huggins <andrewhuggins@gmail.com>
2016-09-05 02:42:14 -0400

    update Menu

--- src/menu.php
+++ src/menu.php
@@ -1,7 +1,12 @@
 <?php
 
 Menu::make('admin-main', function ($menu) {
-    $menu->add('Blog');
-    $menu->add('Create Post', 'admin/blog/create');
-    $menu->add('Categories', 'admin/blog/categories');
+    $menu->add('Blog', ['class' => 'treeview'])
+        ->prepend('<i class="fa fa-list"></i>')
+        ->append('<i class="fa fa-angle-left pull-right"></i>')
+
+        ->link->href('#');
+    // $menu->blog->attr(['class' => 'treeview-menu']);
+    $menu->blog->add('Create Post', ['url' => 'admin/blog/create']);
+    $menu->blog->add('Categories', ['url' => 'admin/blog/categories']);
 });
