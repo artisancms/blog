50fc92bde1e6486b4155d5265988714888744c9a
Andrew huggins <andrewhuggins@gmail.com>
2016-09-17 19:39:03 -0400

    change menu.php so that it works

--- src/menu.php
+++ src/menu.php
@@ -1,12 +1,9 @@
 <?php
 
-Menu::get('admin-main', function ($menu) {
-    $menu->add('Blog', ['class' => 'treeview'])
-        ->prepend('<i class="fa fa-list"></i>')
-        ->append('<i class="fa fa-angle-left pull-right"></i>')
-
-        ->link->href('#');
-    // $menu->blog->attr(['class' => 'treeview-menu']);
-    $menu->blog->add('Create Post', ['url' => 'admin/blog/create']);
-    $menu->blog->add('Categories', ['url' => 'admin/blog/categories']);
-});
+$menu = Menu::get('admin-sidebar');
+$menu->add('Blog', ['class' => 'treeview'])
+    ->prepend('<i class="fa fa-list"></i>')
+    ->append('<i class="fa fa-angle-left pull-right"></i>')
+    ->link->href('#');
+$menu->blog->add('Create Post', ['url' => 'admin/blog/create']);
+$menu->blog->add('Categories', ['url' => 'admin/blog/categories']);
