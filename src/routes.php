d5e4cf5cc6f871fef0db6a016a5193ef75593969
Andrew huggins <andrewhuggins@gmail.com>
2016-09-18 01:08:46 -0400

    adding routes

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,9 @@
 Route::get('blog/', 'ArtisanCMS\Blog\Http\Controllers\BlogController@index');
 Route::get('blog/{slug?}', 'ArtisanCMS\Blog\Http\Controllers\BlogController@show');
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::get('admin/blog/create', 'ArtisanCMS\Blog\Http\Controllers\BlogAdminController@create');
+    Route::post('admin/blog/create', 'ArtisanCMS\Blog\Http\Controllers\BlogAdminController@store');
     Route::get('admin/blog/', 'ArtisanCMS\Blog\Http\Controllers\BlogAdminController@index');
     Route::get('admin/blog/{id}', 'ArtisanCMS\Blog\Http\Controllers\BlogAdminController@edit');
 });
